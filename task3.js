f3ba483d27a95559a3f3194db1cd53e2b0b04eb6
Aminat Bello <aminatbelloa@gmail.com>
2023-10-13 01:10:31 +0100

    created a javascript file

---
+++ task3.js
@@ -0,0 +1,21 @@
+// program to find the largest among three numbers
+const prompt = require ("prompt-sync")({sigint: true})
+// take input from the user
+const num1 = 3;
+const num2 = 5;
+const num3 = 1;
+let largest;
+// check the condition
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+// display the result
+console.log("The largest number is " + largest);
+
+console.log('hello world');
