45822b1fcce00f7c5349870637bda8ed286803fd
Aminat Bello <aminatbelloa@gmail.com>
2023-10-17 22:48:19 +0100

    Rearranged the files in folders and added Module 3 js files

---
+++ Module3/app.js
@@ -0,0 +1,19 @@
+const myFavouriteLanguage = 'Javascript';
+
+// let currentScore = 20;
+
+// currentScore = 30;
+
+// console.log(currentScore);
+
+// console.log(myFavouriteLanguage);
+
+alert(myFavouriteLanguage);
+
+// const age = 30 ;
+// const name = 'Aminat' ;
+// const sentence = 'My name is ' +name+'. I am ' +age+ ' years old'
+
+// console.log(sentence)
+
+
